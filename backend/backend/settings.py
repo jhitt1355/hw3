4f2bb48d3b2a93074bc0b82f3fbc72a85128b31c
Hayden Leachman <haydenleachman@Haydens-MacBook-Pro.local>
2022-04-25 22:46:39 -0400

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
